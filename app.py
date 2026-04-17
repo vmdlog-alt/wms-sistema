2adf0971c8d58db65a07dc9314eb22dcb97dcfd0
vmdlog-alt <vmd.log@gmail.com>
2026-04-17 14:52:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,74 +1,36 @@
-
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 import sqlite3
 
 app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
 
 conn = sqlite3.connect("wms.db", check_same_thread=False)
 cur = conn.cursor()
 
 cur.execute("""
 CREATE TABLE IF NOT EXISTS produtos (
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-nome TEXT,
-familia TEXT,
-estoque INTEGER,
-endereco TEXT,
-picking TEXT
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT,
+    quantidade INTEGER,
+    endereco TEXT
 )
 """)
 conn.commit()
 
-class Produto(BaseModel):
-    nome: str
-    familia: str
-    estoque: int
-    endereco: str
-    picking: str
 
-@app.get("/")
-def home():
-    return {"msg": "WMS Rodando"}
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
-@app.post("/produto")
-def cadastrar(p: Produto):
-    cur.execute("INSERT INTO produtos (nome,familia,estoque,endereco,picking) VALUES (?,?,?,?,?)",
-                (p.nome,p.familia,p.estoque,p.endereco,p.picking))
+
+@app.post("/salvar")
+def salvar(nome: str, quantidade: int, endereco: str):
+    cur.execute(
+        "INSERT INTO produtos (nome, quantidade, endereco) VALUES (?, ?, ?)",
+        (nome, quantidade, endereco)
+    )
     conn.commit()
-    return {"status":"ok"}
-
-@app.get("/produtos")
-def listar():
-    cur.execute("SELECT * FROM produtos")
-    return cur.fetchall()
-
-@app.post("/saida")
-def saida(nome: str, qtd: int):
-    cur.execute("SELECT estoque,picking FROM produtos WHERE nome=?", (nome,))
-    r = cur.fetchone()
-    if not r:
-        return {"erro":"Produto não encontrado"}
-
-    estoque, picking = r
-
-    if qtd > estoque:
-        return {"erro":"Estoque insuficiente"}
-
-    retirada_picking = 0
-    retirada_outros = qtd
-
-    if picking == "SIM" and qtd > 36:
-        retirada_picking = qtd - 36
-        retirada_outros = 36
-
-    novo = estoque - qtd
-    cur.execute("UPDATE produtos SET estoque=? WHERE nome=?", (novo,nome))
-    conn.commit()
-
-    return {
-        "status":"ok",
-        "picking": retirada_picking,
-        "outros": retirada_outros
-    }
-
+    return {"status": "ok"}
